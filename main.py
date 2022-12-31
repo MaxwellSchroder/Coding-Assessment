e65b6d5946f9527c951fa54be8f629e09d50e160
MaxwellSchroder <maxischroder3@gmail.com>
2022-12-31 10:55:56 +1000

    Algorithm works for example case on spec
    Adjusted algorithm to work for the example given on the spec. Now, implementing writing to a file in the correct order.

--- main.py
+++ main.py
@@ -25,10 +25,11 @@ class Buckets:
                 return False
             # compare the new word to the word in listName, if it is smaller than it goes first
             if newName[i] < listName[i]:
+                #print("Name {} is before {} ".format(newName[i], listName[i]))
                 return True
-        # make sure the newName didn't just run out of words
-        if i < len(listName):
-            return True
+            else:
+                return False
+
         # all other cases fail, they must be identical. Either way it doesn't matter
         return True
 
@@ -37,18 +38,21 @@ class Buckets:
         # if the bucket is empty, just add it to the front of the list
         if (len(self.list)) == 0:
             self.list.insert(0, newName)
+            print("First word insert")
         # there are other names. Do insertion sort
         else:
             # insert into correct position by looping through until it is found
             for i in range(len(self.list) + 1):
+                print(i)
                 # if it has already gone through the entire list, just add it to the end
-                if i == len(self.list) + 1:
+                if i == len(self.list):
                     self.list.append(newName)
                     print("this one")
                 else:
                     listName = self.list[i]
                     # check names recursively, if true insert at this position
                     if self.compareNames(newName, listName):
+                        print("Inserting {} before {}".format(newName, listName))
                         self.list.insert(i, newName)
                         break
 
@@ -73,34 +77,42 @@ class Names:
         for i in range(len(self.namesList)):
             self.namesList[i].printy()
 
+    def sortThroughNames(self):
+        # set of alpha values used in names
+        chars = set('abcdefghijklmnopqrstuvwxyz-\n ')
+
+        # read file
+        with open(sys.argv[1], 'r') as my_file:
+            lines = my_file.readlines()
+            for line in lines:
+                line = line.lower()
+                # check that line only contains alphanumeric characters
+                # any((c in chars) for c in s)
+                if all((c in chars) for c in line):
+                    # Find individual words within the name
+                    words = line.split()
+                    # check that we have at least 1 word/name
+                    if len(words) > 0:
+                        lastName = words[-1]
+
+                        # find the index of the last name in the alphabet
+                        firstLetterIndex = ord(lastName[0]) - asciiOffset
+
+                        # rearrange to have lastname first
+                        words.insert(0, lastName)
+                        # remove the lastname at the end of the list
+                        words.pop()
+
+                        # insert that name into the bucket
+                        self.insertNameIntoList(firstLetterIndex, words)
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # create class of sorter
     names = Names()
-    # set of alpha values used in names
-    chars = set('abcdefghijklmnopqrstuvwxyz-\n ')
 
-    # read file
-    with open(sys.argv[1], 'r') as my_file:
-        lines = my_file.readlines()
-        for line in lines:
-            line = line.lower()
-            # check that line only contains alphanumeric characters
-            # any((c in chars) for c in s)
-            if all((c in chars) for c in line):
-                # Find individual words within the name
-                words = line.split()
-                lastName = words[-1]
+    names.sortThroughNames()
 
-                # find the index of the last name in the alphabet
-                firstLetterIndex = ord(lastName[0]) - asciiOffset
 
-                # rearrange to have lastname first
-                words.insert(0, lastName)
-                # remove the lastname at the end of the list
-                words.pop()
-
-                # insert that name into the bucket
-                names.insertNameIntoList(firstLetterIndex, words)
     names.printOrderedNames()
